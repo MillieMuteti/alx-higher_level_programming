0059b9ed283efd9505cd3ad9e84fe7f491496db1
MillieMuteti <milliemuteti@gmail.com>
2022-08-29 10:00:00 -0700

    Fix task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0] if length > 0 else "None"
-    tup = length, first_char
-    return(tup)
-
+    if len(sentence) < 1:
+        first = None
+    else:
+        first = sentence[0]
+    tuple = length, first
+    return tuple
