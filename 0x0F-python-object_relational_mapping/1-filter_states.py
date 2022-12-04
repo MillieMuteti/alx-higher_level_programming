3968fcd06f3a5570e6dba140805468791ada08cf
Millie Muteti <milliemuteti@gmail.com>
2022-12-04 22:54:50 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,14 +6,12 @@ from the database hbtn_0e_0_usa
 from sys import argv
 import MySQLdb
 
-
-if __name__ == '__main__':
-    user, password, database = argv[1], argv[2], argv[3]
-    db = MySQLdb.connect(host="localhost", user=user,
-                         passwd=password, db=database)
-    db = db.cursor()
-    db.execute("""SELECT * FROM states
-    WHERE REGEXP_LIKE(name, '^N', 'c') ORDER BY id""")
-    r = db.fetchall()
-    for i in r:
-        print(i)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
